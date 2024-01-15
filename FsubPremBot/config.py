ab7d7768875a7a8246414211bc28a0e8c5c8414a
D w i i <107328506+dwiiaksa@users.noreply.github.com>
2024-01-15 12:44:30 +0700

    Update config.py

--- FsubPremBot/config.py
+++ FsubPremBot/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-API_ID = int(os.environ.get("API_ID", "9774346"))
-API_HASH = os.environ.get("API_HASH", "a92aed7d74654a563af4b07efbcd88e9")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6246079632:AAGuHanpE6Q-rbgPsFOiHfFyeZvElc8Cnz4")
-LOGS_MAKER_BOT = int(os.environ.get("LOGS_MAKER_BOT", "-1001617596145"))
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://pss:Rextor99@cluster0.ifynkre.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(os.environ.get("OWNER_ID", "968410597"))
+API_ID = int(os.environ.get("API_ID", "24556370"))
+API_HASH = os.environ.get("API_HASH", "a3a701690d9c5f20b45fc063d475c21f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6793568039:AAHcESFGEhS03wvIrY_4lyDtytIJPU8YKVI")
+LOGS_MAKER_BOT = int(os.environ.get("LOGS_MAKER_BOT", "-1002080346445"))
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://Fsubprem:kESK5q156Ccb1tH7@cluster0.ppu2oiz.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(os.environ.get("OWNER_ID", "1768030466"))
